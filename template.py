8fb2ce39767649e00c7eb897ac46eca88068eace
ANSHU <129033345+AnshuML@users.noreply.github.com>
2024-04-04 22:02:15 +0530

    Updated

--- template.py
+++ template.py
@@ -25,6 +25,7 @@ list_of_files = [
     f"{project_name}/pipeline/__init__.py",
     f"{project_name}/pipeline/train_pipeline.py",
     f"{project_name}/pipeline/prediction_pipeline.py",
+    
     "app.py",
     "train.py",
     "requirements.txt",
